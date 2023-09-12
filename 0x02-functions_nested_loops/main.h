d4957c40702c7c3475dceb8571406f5748d748bb
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-09-12 14:10:22 -0700

    trust

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+int _putchar(char c);
+
+void print_a_alphabet(void);
+
+void print_a_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int c);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bawer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
