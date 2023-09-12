d81cba11c1d24c7ff7bb233b69540494a653eea4
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-09-12 13:45:21 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - Prints "_putchar" to stdout
- *
+ * main - Entry point
  * Return: Always 0.
  */
+
 int main(void)
 {
   _putchar('_');
@@ -16,6 +16,5 @@ int main(void)
   _putchar('a');
   _putchar('r');
   _putchar('\n');
-
   return (0);
 }
