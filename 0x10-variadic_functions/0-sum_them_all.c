201809dd29a4d88d78cdecf891eddf95d8a20681
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-12 12:18:05 -0700

    recept

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,22 +3,26 @@
 
 /**
  * sum_them_all - return the sum of all the arguments.
- * @n: number of aguments.
- * @...: list of aguments.
+ * @n: number of arguments.
+ * @...: variable number of arguments.
  *
  * Return: Sum
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list valist;
-    unsigned int i;
-    int sum;
-    if (n == 0)
-        return (0);
-    va_start(valist, n);
-    sum = 0;
-    for (i = 0; i < n; i++)
-        sum += va_arg(valist, int);
-    va_end(valist);
-    return (sum);
+	va_list valist;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+
+	va_end(valist);
+
+	return (sum);
 }
