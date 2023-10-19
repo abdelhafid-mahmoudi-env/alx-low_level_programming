d09ecae1e6e62848af7d98001886bdbb08bb9727
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-19 11:50:00 -0700

    commit lovely

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,15 @@
 #include "lists.h"
+#include <stdio.h>
 
 void weird(void) __attribute__ ((constructor));
 
 /**
  * weird - This function is executed before main.
  *
- * It prints the desired message.
+ * It prints the desired messages.
  */
 void weird(void)
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
-
