7db044ed4727219aeff979394bdd5f7680d0766a
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-19 10:54:35 -0700

    comit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,42 +1,54 @@
 #include <stdio.h>
 #include "lists.h"
 
-void print_node(const list_t *node);
 
 /**
- * print_list - Print all elements in a singly linked list and return the count.
+ * print_list - Prints elements of a singly linked list and returns the count.
  *
- * @h: Head of the list
- * Return: Total number of nodes in the list
+ * @h: The head of the list
+ *
+ * Return: The total number of nodes in the list
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;  /* Initialize a count variable to keep track of nodes. */
+	size_t count = 0;
 
-    if (h == NULL) {
-        return 0; /* Handle the case of an empty list by returning 0. */
-    }
+	if (h == NULL)
+	{
+		return (0);
+	}
 
-    while (h != NULL) {
-        print_node(h);  /* Print the current node. */
-        h = h->next;    /* Move to the next node. */
-        count++;        /* Increment the count for each node processed. */
-    }
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%u] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
 
-    return count; /* Return the total count of nodes in the list. */
+	return (count);
 }
 
 /**
- * print_node - Print a single node from the singly linked list.
+ * print_node - Prints a single node from the singly linked list.
  *
- * @node: Node to be printed
+ * @node: The node to be printed
  */
 void print_node(const list_t *node)
 {
-    /* Print the length and the string (or "(nil)" for NULL strings). */
-    if (node->str == NULL) {
-        printf("[%u] %s\n", 0, "(nil)");
-    } else {
-        printf("[%u] %s\n", node->len, node->str);
-    }
+	if (node->str == NULL)
+	{
+		printf("[%u] %s\n", 0, "(nil)");
+	}
+	else
+	{
+		printf("[%u] %s\n", node->len, node->str);
+	}
 }
+
