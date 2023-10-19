1f8656b26fba92d3a8f0a1ffe69e143fed996f84
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-19 02:45:21 -0700

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,11 +4,11 @@
 #include "lists.h"
 
 /**
- * print_list - Prints all the elements of a list_t list
- * @h: Pointer to the head of the list
- *
- * Return: The number of nodes in the list
- */
+* print_list - Prints all the elements of a list_t list
+* @h: Pointer to the head of the list
+*
+* Return: The number of nodes in the list
+*/
 size_t print_list(const list_t *h)
 {
     size_t count = 0;
