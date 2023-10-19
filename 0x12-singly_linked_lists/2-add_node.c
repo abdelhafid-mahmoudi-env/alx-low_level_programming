68cc10daf4f9e23d88f09fc0b88577fb445f7f01
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-19 11:10:25 -0700

    test

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,40 +1,59 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "lists.h"
 
 /**
- * add_node - function that adds a new node at the beginning of list.
- * @head: pointer to singly linked list.
- * @str: pointer to signly linked list.
+ * create_node - Creates a new node with a duplicated string and length.
  *
- * str needs to be duplicated.
- * You are allowed to use strdup.
- *
- * Return: The address of the new element or NULL if it failed.
- *
+ * @str: The string to be duplicated.
+ * @len: The length of the string.
+ * Return: A pointer to the newly created node.
  */
+list_t *create_node(const char *str, size_t len)
+{
+    list_t *new_node = malloc(sizeof(list_t));
 
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->len = len;
+    new_node->str = strdup(str);
+
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->next = NULL;
+
+    return (new_node);
+}
+
+/**
+ * add_node - Adds a new node at the beginning of a singly linked list.
+ *
+ * @head: A pointer to the pointer of the linked list.
+ * @str: The string to be added to the list.
+ * Return: A pointer to the newly created node or NULL if it fails.
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
-	size_t length = 0;
+    size_t length = 0;
+    list_t *new_node;
 
-	new_node = malloc(sizeof(list_t));
-	/* if it fails returb NULL */
-	if (new_node == NULL)
-		return (NULL);
-	/* loop through the string to find length */
-	while (str[length])
-		length++;
-	/* access the length of new_node and assign it to length */
-	new_node->len = length;
-	/* access the list of new_node and duplicate it */
-	new_node->str = strdup(str);
-	/* access the next node of new_node and assign it as the first node */
-	new_node->next = *head;
-	/* make it the beginning of the list */
-	*head = new_node;
-	/* return it */
-	return (new_node);
+    while (str[length])
+        length++;
+
+    new_node = create_node(str, length);
+
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->next = *head;
+    *head = new_node;
+
+    return (new_node);
 }
+
