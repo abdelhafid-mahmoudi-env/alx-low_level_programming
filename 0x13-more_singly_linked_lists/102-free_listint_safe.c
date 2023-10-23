5f8415dd9b73b28d1b47576a324beba2eab98351
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-23 12:22:30 -0700

    te

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,16 +1,9 @@
 #include "lists.h"
-#include <stdlib.h>
 
-/**
- * free_listint_safe - Frees a listint_t list safely (can free lists containing loops)
- * @h: A pointer to the address of the head of the listint_t list.
- *
- * Return: The size of the list that was freed.
- */
 size_t free_listint_safe(listint_t **h)
 {
-    listint_t *slow, *fast, *tmp;
-    size_t count = 0;
+    listint_t *slow, *fast, *temp;
+    size_t size = 0;
 
     if (h == NULL || *h == NULL)
         return (0);
@@ -18,52 +11,36 @@ size_t free_listint_safe(listint_t **h)
     slow = *h;
     fast = (*h)->next;
 
-    while (fast && fast->next)
+    while (fast != NULL && fast->next != NULL)
     {
         if (slow == fast)
         {
-            /* Detected a loop, break it */
+            /* Loop detected, free the entire list and break */
             slow = *h;
             while (slow != fast)
             {
+                temp = slow;
                 slow = slow->next;
-                fast = fast->next;
-                count++;
+                free(temp);
+                size++;
             }
-
-            slow = fast->next;
-            while (slow != fast)
-            {
-                tmp = slow;
-                slow = slow->next;
-                free(tmp);
-                count++;
-            }
-
-            tmp = slow;
-            slow = slow->next;
-            free(tmp);
-            count++;
-            *h = NULL;
-            break;
+            *h = NULL; /* Set the head to NULL */
+            return (size);
         }
-
         slow = slow->next;
         fast = fast->next->next;
+        size++;
     }
 
-    if (!fast || !fast->next)
+    /* No loop detected, free the list normally */
+    while (*h != NULL)
     {
-        while (*h)
-        {
-            tmp = *h;
-            *h = (*h)->next;
-            free(tmp);
-            count++;
-        }
+        temp = *h;
+        *h = (*h)->next;
+        free(temp);
+        size++;
     }
 
-    h = NULL;
-
-    return (count);
+    return (size);
 }
+
