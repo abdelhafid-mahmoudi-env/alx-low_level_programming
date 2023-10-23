79e3bfdde8b9827eff931fc5a8c15d6b2c0f8389
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-23 12:10:01 -0700

    correct

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,46 +1,30 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * free_listint_safe - Frees a listint_t linked list safely.
- * @h: A pointer to a pointer to the head of the list.
+ * free_listint_safe - Frees a listint_t list safely.
+ * @h: A pointer to the address of the head of the listint_t list.
  *
  * Return: The size of the list that was freed.
+ *
+ * Description: This function sets the head to NULL.
  */
 size_t free_listint_safe(listint_t **h)
 {
-    listint_t *slow, *fast, *temp;
-    size_t count = 0;
+	listint_t *current, *next;
+	size_t nodes = 0;
 
-    slow = fast = *h;
+	if (h == NULL || *h == NULL)
+		return (0);
 
-    while (fast != NULL && fast->next != NULL)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
+	while (*h != NULL)
+	{
+		current = *h;
+		next = current->next;
+		*h = NULL;
+		free(current);
+		*h = next;
+		nodes++;
+	}
 
-        if (slow == fast)
-        {
-            while (*h != slow)
-            {
-                temp = *h;
-                *h = (*h)->next;
-                free(temp);
-                count++;
-            }
-
-            *h = NULL;
-            return (count);
-        }
-    }
-
-    while (*h != NULL)
-    {
-        temp = *h;
-        *h = (*h)->next;
-        free(temp);
-        count++;
-    }
-
-    return (count);
+	return (nodes);
 }
