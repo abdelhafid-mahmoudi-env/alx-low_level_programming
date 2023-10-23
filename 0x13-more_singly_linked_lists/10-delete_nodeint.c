06d1036a92fd2bffa50e0badafcc42016e6b23dc
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-23 11:20:51 -0700

    9 insert node int

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,33 +10,32 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *current, *temp;
-    unsigned int i;
+	listint_t *current, *temp;
+	unsigned int i;
 
-    if (*head == NULL)
-        return -1;
+	if (*head == NULL)
+		return -1;
 
-    if (index == 0)
-    {
-        temp = *head;
-        *head = temp->next;
-        free(temp);
-        return 1;
-    }
+	if (index == 0)
+	{
+		temp = *head;
+		*head = temp->next;
+		free(temp);
+		return 1;
+	}
 
-    current = *head;
-    for (i = 0; current != NULL && i < index - 1; i++)
-    {
-        current = current->next;
-    }
+	current = *head;
+	for (i = 0; current != NULL && i < index - 1; i++)
+	{
+		current = current->next;
+	}
 
-    if (current == NULL || current->next == NULL)
-        return -1;
+	if (current == NULL || current->next == NULL)
+		return -1;
 
-    temp = current->next;
-    current->next = temp->next;
-    free(temp);
+	temp = current->next;
+	current->next = temp->next;
+	free(temp);
 
-    return 1;
+	return (1);
 }
-
