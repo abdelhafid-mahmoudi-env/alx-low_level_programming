c4801190adc34d4f40a91364349b633f83d21290
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-23 12:18:57 -0700

    te

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,30 +1,69 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * free_listint_safe - Frees a listint_t list safely.
+ * free_listint_safe - Frees a listint_t list safely (can free lists containing loops)
  * @h: A pointer to the address of the head of the listint_t list.
  *
  * Return: The size of the list that was freed.
- *
- * Description: This function sets the head to NULL.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *next;
-	size_t nodes = 0;
+    listint_t *slow, *fast, *tmp;
+    size_t count = 0;
 
-	if (h == NULL || *h == NULL)
-		return (0);
+    if (h == NULL || *h == NULL)
+        return (0);
 
-	while (*h != NULL)
-	{
-		current = *h;
-		next = current->next;
-		*h = NULL;
-		free(current);
-		*h = next;
-		nodes++;
-	}
+    slow = *h;
+    fast = (*h)->next;
 
-	return (nodes);
+    while (fast && fast->next)
+    {
+        if (slow == fast)
+        {
+            /* Detected a loop, break it */
+            slow = *h;
+            while (slow != fast)
+            {
+                slow = slow->next;
+                fast = fast->next;
+                count++;
+            }
+
+            slow = fast->next;
+            while (slow != fast)
+            {
+                tmp = slow;
+                slow = slow->next;
+                free(tmp);
+                count++;
+            }
+
+            tmp = slow;
+            slow = slow->next;
+            free(tmp);
+            count++;
+            *h = NULL;
+            break;
+        }
+
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    if (!fast || !fast->next)
+    {
+        while (*h)
+        {
+            tmp = *h;
+            *h = (*h)->next;
+            free(tmp);
+            count++;
+        }
+    }
+
+    h = NULL;
+
+    return (count);
 }
