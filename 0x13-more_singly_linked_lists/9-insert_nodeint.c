74c44373fda29b874a124edc4fffa04441f3f8dc
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-23 11:10:03 -0700

    8 sum list int

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - Inserts a new node at a given position in a listint_t list.
+ * insert_nodeint_at_index - Inserts a new node
+ * at a given position in a listint_t list.
  * @head: A pointer to a pointer to the head of the list.
  * @idx: The index at which to insert the new node (starting at 0).
  * @n: The integer value to store in the new node.
@@ -12,26 +13,25 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    listint_t *new_node, *current = *head;
-    unsigned int i;
+	listint_t *new_node, *current = *head;
+	unsigned int i;
 
-    if (idx == 0)
-    {
-        new_node = add_nodeint(head, n);
-        return new_node;
-    }
+	if (idx == 0)
+	{
+		new_node = add_nodeint(head, n);
+		return (new_node);
+	}
 
-    for (i = 0; i < idx - 1; i++)
-    {
-        if (current == NULL)
-            return NULL;
-        current = current->next;
-    }
+	for (i = 0; i < idx - 1; i++)
+	{
+		if (current == NULL)
+			return (NULL);
+		current = current->next;
+	}
 
-    if (current == NULL)
-        return NULL;
+	if (current == NULL)
+		return (NULL);
 
-    new_node = add_nodeint(&(current->next), n);
-    return new_node;
+	new_node = add_nodeint(&(current->next), n);
+	return (new_node);
 }
-
