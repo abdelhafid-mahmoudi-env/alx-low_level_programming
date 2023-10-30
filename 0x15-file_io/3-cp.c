b23f31fec1f2e3a8b76b5b02af9d603b4d64d332
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-10-30 07:31:31 -0700

    programming

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,6 +7,72 @@
 #define BUFFER_SIZE 1024
 
 /**
+ * check_arguments - Checks the command-line arguments.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the arguments.
+ */
+void check_arguments(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+}
+
+/**
+ * open_file - Opens a file with the specified mode and permissions.
+ * @filename: The name of the file to open.
+ * @mode: The mode to open the file (e.g., O_RDONLY, O_WRONLY).
+ * @permissions: The file permissions to set.
+ * @error_code: The error code to exit with if opening fails.
+ * @read_mode: If true, set the file descriptor
+ * for reading; otherwise, for writing.
+ * @error_message: The error message to print if opening fails.
+ *
+ * Return: The file descriptor on success;
+ * exits with an error code on failure.
+ */
+int open_file(
+		const char *filename,
+		int mode, mode_t permissions,
+		int error_code,
+		int read_mode,
+		const char *error_message
+		)
+{
+	int fd = -1;
+
+	fd = open(filename, mode, permissions);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: %s %s\n", error_message, filename);
+		exit(error_code);
+	}
+
+	if (read_mode)
+	{
+		if (read(fd, NULL, 0) == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+			close(fd);
+			exit(98);
+		}
+	}
+	else
+	{
+		if (write(fd, NULL, 0) == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+			close(fd);
+			exit(99);
+		}
+	}
+
+	return (fd);
+}
+
+/**
  * main - Copies the content of one file to another.
  * @argc: The number of command-line arguments.
  * @argv: An array of strings containing the arguments.
@@ -15,29 +81,17 @@
  */
 int main(int argc, char **argv)
 {
-	int fd_from, fd_to, bytes_read, bytes_written;
+	int fd_from, fd_to;
 	char buffer[BUFFER_SIZE];
+	int bytes_read, bytes_written;
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+	check_arguments(argc, argv);
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
-		exit(99);
-	}
+	fd_from = open_file(argv[1], O_RDONLY, 0, 98, 1, "Can't read from file");
+	fd_to = open_file(
+			argv[2],
+			O_WRONLY | O_CREAT | O_TRUNC,
+			0664, 99, 0, "Can't write to");
 
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
@@ -51,20 +105,8 @@ int main(int argc, char **argv)
 		}
 	}
 
-	if (bytes_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
-		exit(98);
-	}
-
-	if (close(fd_from) == -1 || close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (close(fd_from) == -1) ? fd_from : fd_to);
-		exit(100);
-	}
+	close(fd_from);
+	close(fd_to);
 
 	return (0);
 }
-
