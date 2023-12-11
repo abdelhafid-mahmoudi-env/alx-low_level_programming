2fb1666926db670b6e3d47073db04cc8c2ef2553
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-12-11 12:34:45 -0800

    test

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/gm "https://github.com/alx-tools/0x18.c/raw/master/gm"
+wget -q -O /tmp/gm "https://github.com/alx-tools/0x18.c/raw/master/101-md5_gm"
 export LD_PRELOAD=/tmp/gm
