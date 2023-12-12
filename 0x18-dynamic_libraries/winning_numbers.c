1b37b0a101813c27737d07782bc5777d49edcc65
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-12-11 17:05:15 -0800

    test

--- 0x18-dynamic_libraries/winning_numbers.c
+++ 0x18-dynamic_libraries/winning_numbers.c
@@ -1,26 +1,9 @@
-#include <unistd.h>
-#include <stdlib.h>
+#include <stdio.h>
 
-static int initialized = 0;
-static int counter = 0;
-static const int winning_numbers[] = {9, 8, 10, 24, 75, 9};
-static const int winning_numbers_size = sizeof(winning_numbers) / sizeof(winning_numbers[0]);
-
-void srand(unsigned int seed) {
-    initialized = 1;
-    counter = 0;
-    (void)seed;
+int generate_winning_numbers(int *numbers) {
+  for (int i = 0; i < 5; ++i) {
+   	 numbers[i] = 9 + i;
+  }
+  numbers[5] = 9;
+  return 0;
 }
-
-int rand(void) {
-    if (!initialized) {
-        return random();
-    }
-
-    if (counter < winning_numbers_size) {
-        return winning_numbers[counter++];
-    } else {
-        return random();
-    }
-}
-
