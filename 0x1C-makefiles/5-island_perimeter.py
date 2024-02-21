47fc4383a5cabd1e199f134b9a61f659f1278434
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-02-21 09:10:23 -0800

    test

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""
-Function to calculate the perimeter of an island in a grid.
-"""
+"""Function to calculate the perimeter of an island in a grid."""
+
 
 def island_perimeter(grid):
     """Calculate the perimeter of the island described in grid."""
-    perimeter = 0
-    rows = len(grid)
-    cols = len(grid[0])
+    c = 0
+    rows = len(grid) - 1
+    cols = len(grid[0]) - 1
 
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
-    return perimeter
+    for i, r in enumerate(grid):
+        for j, n in enumerate(r):
+            if n == 1:
+                if i == 0 or grid[i - 1][j] != 1:
+                    c += 1
+                if j == 0 or grid[i][j - 1] != 1:
+                    c += 1
+                if j == cols or grid[i][j + 1] != 1:
+                    c += 1
+                if i == rows or grid[i + 1][j] != 1:
+                    c += 1
+    return c
