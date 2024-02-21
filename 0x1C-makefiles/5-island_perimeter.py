c2c1fc908caabe5cd7208ad18e3aebc3dbbe7bb9
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-02-21 09:08:24 -0800

    test

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,17 +3,8 @@
 Function to calculate the perimeter of an island in a grid.
 """
 
-
 def island_perimeter(grid):
-    """
-    Calculate the perimeter of the island described in grid.
-
-    Args:
-        grid (list of list of int): Grid representing the island.
-
-    Returns:
-        int: Perimeter of the island.
-    """
+    """Calculate the perimeter of the island described in grid."""
     perimeter = 0
     rows = len(grid)
     cols = len(grid[0])
@@ -21,12 +12,9 @@ def island_perimeter(grid):
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += 4  # Each land cell contributes 4 to the perimeter
-
-                # Check neighbors to subtract overlapping sides
+                perimeter += 4
                 if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2  # Subtract 2 if the upper neighbor is also land
+                    perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2  # Subtract 2 if the left neighbor is also land
-
+                    perimeter -= 2
     return perimeter
