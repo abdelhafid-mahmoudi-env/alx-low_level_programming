af130ee57f9917d672a15e2e2e77824caf8d2f0e
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-10 08:49:52 -0700

    test

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -17,18 +17,18 @@ int exponential_search(int *array, size_t size, int value)
 		return (-1);
 
 	for (i = 1; i < size && array[i] < value; i <<= 1)
-        {
-                printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        }
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+	}
 
-        newsize = (i >= size ? size : i + 1) - (i >> 1);
-        i >>= 1;
+	newsize = (i >= size ? size : i + 1) - (i >> 1);
+	i >>= 1;
 
-        printf("Value found between indexes [%lu] and [%lu]\n",
-                i, i << 1 >= size ? size - 1 : i << 1);
+	printf("Value found between indexes [%lu] and [%lu]\n",
+		   i, i << 1 >= size ? size - 1 : i << 1);
 
-        ret = binary_search(array + i, newsize, value);
-        return (ret == -1 ? ret : ret + (int)i);
+	ret = binary_search(array, i + 1, value);
+	return (ret == -1 ? ret : ret + (int)i);
 }
 
 /**
@@ -43,33 +43,33 @@ int exponential_search(int *array, size_t size, int value)
  */
 int binary_search(int *array, size_t size, int value)
 {
-        size_t left = 0, right = size - 1, mid;
+	size_t left = 0, right = size - 1, mid;
 
-        if (array == NULL)
-                return (-1);
+	if (array == NULL)
+		return (-1);
 
-        while (left <= right)
-        {
-                size_t i;
+	while (left <= right)
+	{
+		size_t i;
 
-                printf("Searching in array: ");
-                for (i = left; i <= right; i++)
-                {
-                        printf("%d", array[i]);
-                        if (i < right)
-                                printf(", ");
-                }
-                printf("\n");
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i < right)
+				printf(", ");
+		}
+		printf("\n");
 
-                mid = (left + right) / 2;
+		mid = (left + right) / 2;
 
-                if (array[mid] == value)
-                        return (mid);
-                else if (array[mid] < value)
-                        left = mid + 1;
-                else
-                right = mid - 1;
-        }
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
 
-        return (-1);
+	return (-1);
 }
