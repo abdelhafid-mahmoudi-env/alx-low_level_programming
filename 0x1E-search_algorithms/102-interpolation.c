842ddd308da7f9deb572c10dcc3969f6b6267dc5
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-10 08:19:27 -0700

    test

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,13 +12,14 @@
 int interpolation_search(int *array, size_t size, int value)
 {
 	size_t low = 0, high = size - 1;
+	size_t pos;
 
 	if (array == NULL)
 		return (-1);
 
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
-		size_t pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
 
 		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
