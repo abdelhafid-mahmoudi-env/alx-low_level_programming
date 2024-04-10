42d6aa0a166da1e5409101fe5a268dde8abbccfc
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-10 10:28:21 -0700

    test

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,33 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "search_algos.h"
 
 /**
- * linear_skip - Searches for a value in a sorted skip list of integers
- * @list: Pointer to the head of the skip list to search in
- * @value: Value to search for
+ * linear_skip - searches for a value in a sorted skip list of integers
+ * @list: pointer to the head of the skip list
+ * @value: value to search for
  *
- * Return: Pointer to the first node where value is located.
+ * Return: pointer to the node with the desired value.
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *current = list, *express;
+	skiplist_t *current = list, *express_node = NULL, *last_node;
 
-	if (list == NULL)
+	if (!list)
 		return (NULL);
 
-	express = list->express;
-	while (express != NULL && express->n < value)
+	while (current->express && current->n < value)
 	{
-		printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
-		current = express;
-		express = express->express;
+		express_node = current->express;
+		printf(
+				"Value checked at index [%lu] = [%d]\n",
+				express_node->index, express_node->n);
+		if (express_node->n >= value)
+			break;
+		current = express_node;
 	}
 
-	printf("Value found between indexes [%lu] and [%lu]\n",
-		   current->index, express ? express->index : current->index);
+	if (!current->express)
+	{
+		last_node = current;
+		while (last_node->next)
+			last_node = last_node->next;
+		printf("Value found between indexes [%lu] and [%lu]\n",
+			   current->index, last_node->index);
+		current = last_node;
+	}
+	else
+	{
+		printf("Value found between indexes [%lu] and [%lu]\n",
+			   current->index, express_node->index);
+	}
 
-	while (current != NULL && current->n <= value)
+	while (current)
 	{
 		printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
 		if (current->n == value)
